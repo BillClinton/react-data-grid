a758045d2a168a9d64d73ebbdf4914e4771df07d
BillClinton <bclinton@appunto.net>
2019-12-11 14:21:03 -0500

    Separate styles into different modules

--- src/components/UserCard.js
+++ src/components/UserCard.js
@@ -1,39 +1,45 @@
 import React, { useContext } from 'react';
 import { UserStore } from '../contexts/UserStore';
 import { GridContext } from './grid/DataGrid';
+import styles from './UserCard.module.scss';
 
 const UserCard = () => {
   const { store } = useContext(UserStore);
   const { selectedIndex } = useContext(GridContext);
 
-  console.log(selectedIndex);
-
   let rec = store.data[selectedIndex];
 
-  return (
-    <div>
-      {selectedIndex >= 0 && (
-        <>
-          <div>
-            <div>{rec.name}</div>
-            <div>{rec.username}</div>
-            <div>{rec.email}</div>
-            <div>{rec.phone}</div>
-            <div>{rec.website}</div>
-            <div>{rec.address.street}</div>
-            <div>{rec.address.suite}</div>
-            <div>{rec.address.city}</div>
-            <div>{rec.address.zipcode}</div>
-          </div>
-          <div>
-            <div>{rec.company.name}</div>
-            <div>{rec.company.catchphrase}</div>
-            <div>{rec.company.bs}</div>
-          </div>
-        </>
-      )}
+  return selectedIndex >= 0 && rec ? (
+    <div className={styles.infoCardWrap}>
+      <h1 className={styles.cardHeader}>{rec.name}</h1>
+      <div className={styles.infoCard}>
+        <div className={styles.userSection}>
+          <h2>Personal</h2>
+          <div>username:</div>
+          <div>{rec.username}</div>
+          <div>email:</div>
+          <div>{rec.email}</div>
+          <div>phone:</div>
+          <div>{rec.phone}</div>
+          <div>website:</div>
+          <div>{rec.website}</div>
+        </div>
+        <div className={styles.addressSection}>
+          <h2>Address</h2>
+          <div>{rec.address.street}</div>
+          <div>{rec.address.suite}</div>
+          <div>{rec.address.city}</div>
+          <div>{rec.address.zipcode}</div>
+        </div>
+        <div className={styles.companySection}>
+          <h2>Company</h2>
+          <div>{rec.company.name}</div>
+          <div>{rec.company.catchPhrase}</div>
+          <div>{rec.company.bs}</div>
+        </div>
+      </div>
     </div>
-  );
+  ) : null;
 };
 
 export default UserCard;
--- src/components/UserGrid.js
+++ src/components/UserGrid.js
@@ -1,37 +1,37 @@
 import React, { useContext } from 'react';
 import { UserStore } from '../contexts/UserStore';
 import DataGrid from './grid/DataGrid';
+import UserCard from './UserCard';
 
 const UserGrid = () => {
   const { store } = useContext(UserStore);
 
+  const columns = [
+    {
+      text: 'Name',
+      dataIndex: 'name',
+      span: 3
+    },
+    {
+      text: 'Email',
+      dataIndex: 'email',
+      span: 4
+    },
+    {
+      text: 'Company',
+      dataIndex: 'company.name',
+      span: 3
+    },
+    {
+      text: 'City',
+      dataIndex: 'address.city',
+      span: 1
+    }
+  ];
+
   return (
-    <DataGrid
-      store={store}
-      columns={[
-        {
-          text: 'Name',
-          dataIndex: 'name',
-          span: 3
-        },
-        {
-          text: 'Email',
-          dataIndex: 'email',
-          span: 4
-        },
-        {
-          text: 'Company',
-          dataIndex: 'company.name',
-          span: 3
-        },
-        {
-          text: 'City',
-          dataIndex: 'address.city',
-          span: 2
-        }
-      ]}
-    >
-      <div>Card content goes here</div>
+    <DataGrid store={store} columns={columns}>
+      <UserCard />
     </DataGrid>
   );
 };
