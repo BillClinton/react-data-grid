45796cb8380fd3c9bace13c6739b57a727d0a5ae
BillClinton <bclinton@appunto.net>
2019-12-13 03:34:53 -0500

    Add a link to repo for demo

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import UserGrid from './components/UserGrid';
 import UserStoreProvider from './contexts/UserStore';
+import './App.scss';
 
 function App() {
   return (
@@ -8,6 +9,12 @@ function App() {
       <UserStoreProvider>
         <UserGrid />
       </UserStoreProvider>
+      <div className="repo-link">
+        repo:{' '}
+        <a href="https://github.com/BillClinton/react-data-grid">
+          https://github.com/BillClinton/react-data-grid
+        </a>
+      </div>
     </div>
   );
 }
